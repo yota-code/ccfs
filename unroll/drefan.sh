7b612ce650c188ce3dfdd840174478bb0a5275c7
yoochan <yota.news@gmail.com>
2020-12-18 14:56:37 +0100

    eod

---
+++ unroll/drefan.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+source ${0:A:h:h}/install_path /C/adstd/upsoft2/root/platform/debian-8.3_ivybridge_2020.12
+
+ccfs_standard_install.sh openssl-1.1.1h
+ccfs_standard_install.sh Python-3.8.6
